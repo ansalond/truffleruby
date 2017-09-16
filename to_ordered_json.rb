dd9523bf42d83a9305f18f50593e36437d658ecc
Benoit Daloze <eregontp@gmail.com>
2017-09-17 00:29:48 +0200

    changes to ci.hocon to ease comparison

---
+++ to_ordered_json.rb
@@ -0,0 +1,26 @@
+require 'json'
+
+builds = JSON.load(STDIN.read)["builds"]
+
+$stderr.puts builds.size
+data = builds#[0...68]
+
+def sort_hash(hash)
+  hash.map { |k,v|
+    if Hash === v
+      v = sort_hash(v)
+    end
+    [k, v]
+  }.sort_by { |k,v| k }.to_h
+end
+
+data = data.map { |job|
+  job = sort_hash(job)
+  job.delete "build"
+  job.delete "prelude"
+  job.delete "run_benchs"
+  # job["environment"] = sort_hash(job["environment"])
+  job
+}
+
+puts JSON.pretty_generate(data)
